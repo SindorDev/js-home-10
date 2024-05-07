c9af74a11c1ceb4cb8f27e05bf009d3527e47e4d
Sindor007 <thesso003@gmail.com>
2024-05-07 10:05:15 +0500

    HomeWork complete

--- Page 4/app.js
+++ Page 4/app.js
@@ -1,1 +1,15 @@
-let
+function flickSwitch(arr){
+     let result = [];
+     let defaultValue = true
+     arr.forEach(word => {
+         if(word === "flick"){
+             defaultValue = !defaultValue
+         }
+         result.push(defaultValue);
+     });
+     return result
+ }
+ 
+ let result = flickSwitch(['codewars', 'flick', 'code', 'wars', "flick", "apple"]);
+ 
+ console.log(result);
